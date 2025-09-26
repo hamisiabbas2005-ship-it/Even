6c4bec6ea595f784cf2a2c82aa5e2bc885d9c094
HAMISI <hamisiabbas2005@gmail.com>
2025-09-26 12:56:12 +0300

    Add files via upload

---
+++ hamisiabbas.js
@@ -0,0 +1,33 @@
+//I'm declaring my function here
+function isEven(num){
+if (num % 2 === 0){
+   return true;
+} else {
+   return false;
+}
+}
+
+//Test cases
+console.log(isEven(4)); //Expected output: true
+console.log(isEven(7)); //Expected output: false
+console.log(isEven(-2)); //Expected output: true
+
+
+@hamisiabbas2005-ship-it ➜ /workspaces/Even (main) $ nano func.js
+@hamisiabbas2005-ship-it ➜ /workspaces/Even (main) $ node func.js
+@hamisiabbas2005-ship-it ➜ /workspaces/Even (main) $ cat func.js
+//I'm declaring my function here
+function isEven(num){
+if (num % 2 === 0){
+   return true;
+} else {
+   return false;
+}
+}
+@hamisiabbas2005-ship-it ➜ /workspaces/Even (main) $ nano func.js
+@hamisiabbas2005-ship-it ➜ /workspaces/Even (main) $ nano func.js
+@hamisiabbas2005-ship-it ➜ /workspaces/Even (main) $ node func.js
+true
+false
+true
+@hamisiabbas2005-ship-it ➜ /workspaces/Even (main) $ 
